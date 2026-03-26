70d421832425453bcedf23cae44f261ac8438590
lmcpheee <ctx2thfgbq@privaterelay.appleid.com>
2026-03-26 16:22:02 -0600

    Lydia Move Class

---
+++ move_class_Lydia.py
@@ -0,0 +1,14 @@
+import random
+
+class Move:
+    def __init__(self, move_name, elemental_type, low_attack_points, high_attack_points):
+        self.move_name = move_name
+        self.elemental_type = elemental_type
+        self.low_attack_points = low_attack_points
+        self.high_attack_points = high_attack_points
+
+    def get_info(self):
+        return f"{self.move_name} (Type: {self.elemental_type}): {self.low_attack_points} to {self.high_attack_points} Attack Points"
+
+    def generate_attack_value(self):
+        return random.randint(self.low_attack_points, self.high_attack_points)
